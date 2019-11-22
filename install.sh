6e85cb43ebfe82ce1590a4411cccd284e4cb64fc
ParcelSolutions <info@parcelsolutions.eu>
2019-11-22 12:32:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -122,9 +122,10 @@ then
 	echo "=> docker"
 		sudo apt install -y --no-install-recommends apt-transport-https ca-certificates curl software-properties-common
 		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-		sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-		curl -fsSL https://get.docker.com -o get-docker.sh
-		sh get-docker.sh
+		sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+		sudo apt-get update
+		#apt-cache policy docker-ce
+		sudo apt-get install -y docker-ce
 	
 
 	
