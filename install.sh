e2fb62f01d5596aed1441ef76d3b47fbdd47affb
ParcelSolutions <info@parcelsolutions.eu>
2019-12-05 14:59:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -57,8 +57,18 @@ then
 		ibmcloud plugin install cloud-databases -f
 		ibmcloud -v 
 
+	echo "=> install mongoclient"
+		wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+		#if needed
+		#sudo mkdir /etc/apt/sources.list.d
+		#sudo chmod 755 /etc/apt/sources.list.d
+		echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+		sudo apt-get update
+		sudo apt-get install -y mongodb-org
+		
 	echo "=> build Meteor"
 	  	curl https://install.meteor.com | /bin/sh &&  meteor --version 
+				
 
 
 
